825b2a3e6d27d9f7329b373d83e3a8ae258ebc5f
majidyavari20669-web <majidyavari20669@gmail.com>
2025-08-11 13:13:52 +0330

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,3 +1,41 @@
-self.addEventListener('install', () => {
-  self.skipWaiting();
+const CACHE_NAME = 'jassaz-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/icons/icon-192.png',
+  '/icons/icon-512.png'
+];
+
+// نصب Service Worker و کش کردن فایل‌ها
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+// فعال‌سازی Service Worker و پاک کردن کش‌های قدیمی
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
+
+// واکشی درخواست‌ها
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
 });
